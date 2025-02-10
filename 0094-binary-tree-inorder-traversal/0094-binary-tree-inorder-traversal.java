edae08a2571e6e06cc597bf89262ec850b7f2207
Kalyan Ram <117086788+kalyanram003@users.noreply.github.com>
2025-02-11 00:02:17 +0530

    Time: 0 ms (100%), Space: 41.9 MB (27.14%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -16,11 +16,35 @@
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> ans=new ArrayList<>();
-        if(root!=null){
-            ans.addAll(inorderTraversal(root.left));
-            ans.add(root.val);
-            ans.addAll(inorderTraversal(root.right));
+        Stack<TreeNode> st=new Stack<>();
+        TreeNode node=root;
+        while(true){
+            if(node!=null){
+                st.push(node);
+                node=node.left;
+            }
+            else{
+                if(st.isEmpty()){
+                    break;
+                }
+                node=st.pop();
+                ans.add(node.val);
+                node=node.right;
+            }
         }
         return ans;
     }
 }
+
+//using recursion
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> ans=new ArrayList<>();
+//         if(root!=null){
+//             ans.addAll(inorderTraversal(root.left));
+//             ans.add(root.val);
+//             ans.addAll(inorderTraversal(root.right));
+//         }
+//         return ans;
+//     }
+// }
