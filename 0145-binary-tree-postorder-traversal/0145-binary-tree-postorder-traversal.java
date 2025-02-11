a0d6fd995a1609faea96a5e3f36f8b017a9886bf
Kalyan Ram <117086788+kalyanram003@users.noreply.github.com>
2025-02-11 10:45:23 +0530

    Time: 1 ms (7.82%), Space: 41.5 MB (91.42%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -13,14 +13,42 @@
  *     }
  * }
  */
+
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
+        Stack<TreeNode> st1=new Stack<>();
+        Stack<TreeNode> st2=new Stack<>();
         List<Integer> ans=new ArrayList<>();
-        if(root!=null){
-            ans.addAll(postorderTraversal(root.left));
-            ans.addAll(postorderTraversal(root.right));
-            ans.add(root.val);
+        if(root==null){
+            return ans;
+        }
+        st1.push(root);
+        while(!st1.isEmpty()){
+            root=st1.pop();
+            st2.push(root);
+            if(root.left!=null){
+                st1.push(root.left);
+            }
+            if(root.right!=null){
+                st1.push(root.right);
+            }
+            
+        }
+        while(!st2.isEmpty()){
+            ans.add(st2.pop().val);
         }
         return ans;
     }
 }
+
+// class Solution {
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         List<Integer> ans=new ArrayList<>();
+//         if(root!=null){
+//             ans.addAll(postorderTraversal(root.left));
+//             ans.addAll(postorderTraversal(root.right));
+//             ans.add(root.val);
+//         }
+//         return ans;
+//     }
+// }
