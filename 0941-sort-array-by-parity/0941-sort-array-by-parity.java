df403216e37e81654db2307e2852f2b3ce63abe2
Kalyan Ram <117086788+kalyanram003@users.noreply.github.com>
2025-03-30 16:00:40 +0530

    Time: 0 ms (100%), Space: 45 MB (42.54%) - LeetHub

---
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int i=0;
+        if(nums.length==1){
+            return nums;
+        }
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]%2==0){
+                swap(nums,i,j);
+                i++;
+            }
+        }
+        return nums;
+    }
+        public void swap(int arr[],int i,int j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+}
