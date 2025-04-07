5e6f3fa9ecf0d8a5171bebc9284fcdc22c2b7851
Kalyan Ram <117086788+kalyanram003@users.noreply.github.com>
2025-04-07 21:14:26 +0530

    Time: 2 ms (98.48%), Space: 45.4 MB (19.32%) - LeetHub

--- 0979-di-string-match/0979-di-string-match.java
+++ 0979-di-string-match/0979-di-string-match.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] diStringMatch(String s) {
         int n=s.length();
-        int i=0;
-        int j=n;
+        int low=0;
+        int st=n;
         int[] ans=new int[n+1];
         for(int k=0;k<n;k++){
             if(s.charAt(k)=='I'){
-                ans[k]=i++;
+                ans[k]=low++;
             }else{
-                ans[k]=j--;
+                ans[k]=st--;
             }
         }
-        ans[n]=i;
+        ans[n]=low;
         return ans;
     }
 }
